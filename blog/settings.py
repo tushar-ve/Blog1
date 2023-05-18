f8a5151ce004d06e9e4b58812f3d09f55efe9715
tushar-ve <132069412+tushar-ve@users.noreply.github.com>
2023-05-18 18:53:22 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -155,6 +155,6 @@ EMAIL_USE_TLS=True
 
 EMAIL_PORT=587
 
-EMAIL_HOST_USER="nirwalriya51@gmail.com"
+EMAIL_HOST_USER="-----------"
 
-EMAIL_HOST_PASSWORD="tdriimpmokizolbf"
+EMAIL_HOST_PASSWORD="--------------"
