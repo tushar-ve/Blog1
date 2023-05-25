7297d6cb6f2851a896f1c1be0afe62988b9e167e
Tushar <tusharsharma@virtualemployee.com>
2023-05-25 13:01:59 +0530

    adding sqllite

--- blog/settings.py
+++ blog/settings.py
@@ -81,25 +81,31 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+
+#   'default': {
+
+#     'ENGINE': 'django.db.backends.postgresql',
+
+#     'NAME': 'Lekh',
+
+#     'USER': 'postgres',
+
+#     'PASSWORD': 'admin',
+
+#     'HOST': 'localhost',
+
+#     'PORT': '5432',
+
+#   }
+
+# }
 DATABASES = {
-
-  'default': {
-
-    'ENGINE': 'django.db.backends.postgresql',
-
-    'NAME': 'Lekh',
-
-    'USER': 'postgres',
-
-    'PASSWORD': 'admin',
-
-    'HOST': 'localhost',
-
-    'PORT': '5432',
-
-  }
-
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 
 # Password validation
